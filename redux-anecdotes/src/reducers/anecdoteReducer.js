b42cd94249805dc27108db39be0f32dfa798ab46
Igor071 <iburykin071@gmail.com>
2023-12-29 23:36:30 +0200

    6.3: anecdotes, step1

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,16 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      return state.map(anecdote => {
+        if (anecdote.id === action.payload.id) anecdote.votes += 1
+        return anecdote
+      })
+
+    default:
+      return state
+  }
 }
 
 export default reducer
